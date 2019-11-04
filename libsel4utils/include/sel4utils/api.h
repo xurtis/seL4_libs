9534be1d0a210533becdd0b481cabcdce8ae8c3e
Curtis Millar <curtis.millar@data61.csiro.au>
2019-11-05 10:26:19 +1100

    kimage: add wrapper for kernel image bind
    This allows KernelImage_Bind to be used without kernel image
    configuration.

--- libsel4utils/include/sel4utils/api.h
+++ libsel4utils/include/sel4utils/api.h
@@ -65,3 +65,13 @@ static inline seL4_Error api_kernel_image_clone(UNUSED seL4_CPtr dest, UNUSED se
     return seL4_KernelImage_Clone(dest, src);
 #endif
 }
+
+static inline seL4_Error api_kernel_image_bind(UNUSED seL4_CPtr image, UNUSED seL4_CPtr vspace)
+{
+    if (!config_set(CONFIG_KERNEL_IMAGES)) {
+        return (seL4_Error) - ENOSYS;
+    }
+#ifdef CONFIG_KERNEL_IMAGES
+    return seL4_KernelImage_Bind(image, vspace);
+#endif
+}

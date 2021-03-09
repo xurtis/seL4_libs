e33ac62ed9cfc342fcdfd14deeaabe31a934142c
Curtis Millar <curtis.millar@data61.csiro.au>
2021-03-10 10:44:32 +1100

    Add more logging events

--- libsel4debug/src/logbuffer.c
+++ libsel4debug/src/logbuffer.c
@@ -16,6 +16,7 @@
 #include <sel4debug/logbuffer.h>
 #include <utils/base64.h>
 #include <utils/cbor64.h>
+#include <utils/zf_log.h>
 
 /* Strings tracked and compressed in the string domain */
 char *identifiers[] = {
@@ -25,6 +26,7 @@ char *identifiers[] = {
 
     /* None event */
     "None",
+    "virtual-address"
 
     /* Entry and exit events */
     "Entry",
@@ -55,6 +57,21 @@ char *identifiers[] = {
     "microseconds",
     "cycles",
 
+    /* IRQ events */
+    "Irq",
+    "irq",
+
+    /* Invocations and syscalls */
+    "Syscall",
+    "Invocation",
+    "syscall",
+    "label",
+    "cptr",
+
+    /* Thread names */
+    "ThreadName",
+    "name",
+
     /* NULL array terminator */
     NULL,
 };
@@ -68,7 +85,7 @@ char *identifiers[] = {
 
 /* Number of fields in event other than the type fields */
 size_t field_count[seL4_NumLogTypeIds] = {
-    [seL4_Log_TypeId(None)] = 0,
+    [seL4_Log_TypeId(None)] = 1,
     [seL4_Log_TypeId(Entry)] = SMP_COND(2, 1),
     [seL4_Log_TypeId(Exit)] = SMP_COND(2, 1),
     [seL4_Log_TypeId(Block)] = 2,
@@ -77,6 +94,10 @@ size_t field_count[seL4_NumLogTypeIds] = {
     [seL4_Log_TypeId(SwitchThread)] = SMP_COND(2, 1),
     [seL4_Log_TypeId(SwitchSchedContext)] = SMP_COND(2, 1),
     [seL4_Log_TypeId(Timestamp)] = SMP_COND(3, 2),
+    [seL4_Log_TypeId(Irq)] = SMP_COND(2, 1),
+    [seL4_Log_TypeId(Syscall)] = 1,
+    [seL4_Log_TypeId(Invocation)] = 2,
+    [seL4_Log_TypeId(ThreadName)] = 2,
 };
 
 /*
@@ -93,6 +114,9 @@ static int event_cbor64(seL4_LogEvent *event, cbor64_domain_t *domain, base64_t 
     switch (event_type) {
     case seL4_Log_TypeId(None): {
         cbor64_utf8_ref(streamer, domain, "None");
+
+        cbor64_utf8_ref(streamer, domain, "virtual-address");
+        cbor64_uint(streamer, (seL4_Word)event);
         break;
     }
 
@@ -222,6 +246,53 @@ static int event_cbor64(seL4_LogEvent *event, cbor64_domain_t *domain, base64_t 
         break;
     }
 
+    case seL4_Log_TypeId(Irq): {
+        seL4_Log_Type(Irq) *irq = seL4_Log_Cast(Irq)event;
+        cbor64_utf8_ref(streamer, domain, "Irq");
+
+#ifdef SMP_ENABLED
+        cbor64_utf8_ref(streamer, domain, "cpu-id");
+        cbor64_uint(streamer, event->data);
+#endif
+
+        cbor64_utf8_ref(streamer, domain, "irq");
+        cbor64_uint(streamer, irq->irq);
+        break;
+    }
+
+    case seL4_Log_TypeId(Syscall): {
+        seL4_Log_Type(Syscall) *syscall = seL4_Log_Cast(Syscall)event;
+        cbor64_utf8_ref(streamer, domain, "Syscall");
+
+        cbor64_utf8_ref(streamer, domain, "syscall");
+        cbor64_int(streamer, (long)syscall->syscall);
+        break;
+    }
+
+    case seL4_Log_TypeId(Invocation): {
+        seL4_Log_Type(Invocation) *inv = seL4_Log_Cast(Invocation)event;
+        cbor64_utf8_ref(streamer, domain, "Invocation");
+
+        cbor64_utf8_ref(streamer, domain, "label");
+        cbor64_uint(streamer, event->data);
+
+        cbor64_utf8_ref(streamer, domain, "cptr");
+        cbor64_uint(streamer, inv->cptr);
+        break;
+    }
+
+    case seL4_Log_TypeId(ThreadName): {
+        seL4_Log_Type(ThreadName) *thread = seL4_Log_Cast(ThreadName)event;
+        cbor64_utf8_ref(streamer, domain, "ThreadName");
+
+        cbor64_utf8_ref(streamer, domain, "thread");
+        cbor64_uint(streamer, thread->thread);
+
+        cbor64_utf8_ref(streamer, domain, "name");
+        cbor64_utf8_ref(streamer, domain, thread->name);
+        break;
+    }
+
     default: {
         cbor64_utf8_ref(streamer, domain, "Unknown");
         break;
@@ -245,6 +316,7 @@ int debug_log_buffer_dump_cbor64(seL4_LogBuffer *buffer, base64_t *streamer)
 
     /* Create a copy of the log buffer to traverse the events */
     seL4_LogBuffer cursor = *buffer;
+    cursor.index = 0;
 
     cbor64_array_start(streamer);
     seL4_LogEvent *event = seL4_LogBuffer_next(&cursor);
@@ -256,5 +328,24 @@ int debug_log_buffer_dump_cbor64(seL4_LogBuffer *buffer, base64_t *streamer)
 
         event = seL4_LogBuffer_next(&cursor);
     }
+
+#if 0
+    if (cursor.index < cursor.size) {
+        ZF_LOGE(
+            "Found zero-length log item of type #%d at %d",
+            seL4_LogEvent_type(seL4_LogBuffer_event(cursor, cursor.index)),
+            cursor.index
+        );
+        return -1;
+    } else if (cursor.index > cursor.size) {
+        ZF_LOGE(
+            "Overran buffer by %d to %d",
+            cursor.index - cursor.size,
+            cursor.index
+        );
+        return -1;
+    }
+#endif
+
     return cbor64_array_end(streamer);
 }
